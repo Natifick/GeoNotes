aa21ca9c7aa0a8596b52e3fe0f938d6c3698e87d
Nikita Khoroshavtsev <nikitakhor@gmail.com>
2021-03-23 17:13:12 +0300

    Cleaned MapsActivity class, now it always has a marker on the map

--- app/src/main/java/com/natifick/geonotes/MainActivity.java
+++ app/src/main/java/com/natifick/geonotes/MainActivity.java
@@ -1,12 +1,31 @@
 package com.natifick.geonotes;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    // remember the user's Marker
+    LatLng MarkerPoint;
+    private static final String MARKER = "marker";
+
+    // to decode geolocation of Marker
+    Geocoder geocoder=new Geocoder(this);
+
+    // for logs
+    private static final String TAG = MapsActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +36,22 @@ public class MainActivity extends AppCompatActivity {
     public void MakeNewPlace(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data == null) return;
+        if (resultCode == RESULT_OK) {
+            MarkerPoint = data.getParcelableExtra(MARKER);
+            try{
+                ((TextView) findViewById(R.id.UsersPosition)).setText(
+                        geocoder.getFromLocation(MarkerPoint.latitude,
+                                MarkerPoint.longitude, 1).get(0).toString());
+            }
+            catch (IOException ex){
+                Log.e("geocoder", ex.getMessage());
+            }
+        }
     }
 }
--- app/src/main/java/com/natifick/geonotes/MapsActivity.java
+++ app/src/main/java/com/natifick/geonotes/MapsActivity.java
@@ -5,13 +5,13 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.util.Log;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -20,55 +20,30 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.PlaceLikelihood;
-import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
-import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
-import com.google.android.libraries.places.api.net.PlacesClient;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-
-import static android.provider.SettingsSlicesContract.KEY_LOCATION;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap map;
 
-    // remember the user's marker
-    LatLng ChosenPoint;
-    // to decode geolocation of marker
-    Geocoder geocoder=new Geocoder(this);
 
     // client for map and location
     FusedLocationProviderClient fusedLocationClient;
 
-    // Access to the Places API
-    private PlacesClient placesClient;
-
     // to find users current location
-    Location lastKnownLocation;
     private static final int DEFAULT_ZOOM = 15;
     private static final String KEY_CAMERA_POSITION = "camera_position";
-    private CameraPosition cameraPosition;
-
-    public static final int N_MAX_ENTRIES = 5;
-    private String[] likelyPlaceNames;
-    private String[] likelyPlaceAddresses;
-    private List[] likelyPlaceAttributions;
-    private LatLng[] likelyPlaceLatLngs;
 
     // default location is, of course, our favorite university
     private final LatLng defaultLocation = new LatLng(55.669949, 37.481132);
+
+    // remember the user's Marker
+    LatLng MarkerPoint = defaultLocation;
+    private static final String MARKER = "marker";
 
     // for logs
     private static final String TAG = MapsActivity.class.getSimpleName();
@@ -82,8 +57,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         if (savedInstanceState != null){
-            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
-            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+            MarkerPoint = savedInstanceState.getParcelable(MARKER);
         }
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -98,14 +72,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         if (map != null) {
             outState.putParcelable(KEY_CAMERA_POSITION, map.getCameraPosition());
-            outState.putParcelable(KEY_LOCATION, lastKnownLocation);
+            outState.putParcelable(MARKER, MarkerPoint);
         }
         super.onSaveInstanceState(outState);
+    }
+
+    /**
+     * Возвращает выбранную геолокацию
+     * @param view - наша кнопка для завершения активности
+     */
+    public void returnMarker(View view){
+        Intent intent = new Intent();
+        intent.putExtra(MARKER, MarkerPoint);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 
     /**
@@ -125,108 +109,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.option_get_place) {
-            showCurrentPlace();
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void openPlacesDialog(){
-
-    }
-
-    /**
-     * to get places where the user could be, and show the most likely of them
-     */
-    private void showCurrentPlace() {
-        if (map==null){
-            return;
-        }
-
-        if (locationPermissionGranted){
-            // specify the data type to return by place.fields
-            List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS,
-                    Place.Field.LAT_LNG);
-            // builder for FindCurrentPlaceRequest
-            FindCurrentPlaceRequest request =
-                    FindCurrentPlaceRequest.newInstance(placeFields);
-            // Get the most likely places where the device could be
-            // Houses, and some other points of interest
-            @SuppressWarnings("MissingPermission") final
-            Task<FindCurrentPlaceResponse> placeResult =
-                    placesClient.findCurrentPlace(request);
-            // listener is called when the Task is complete
-            placeResult.addOnCompleteListener (new OnCompleteListener<FindCurrentPlaceResponse>() {
-                @Override
-                public void onComplete(@NonNull Task<FindCurrentPlaceResponse> task) {
-                    if (task.isSuccessful() && task.getResult() != null) {
-                        FindCurrentPlaceResponse likelyPlaces = task.getResult();
-
-                        // Handle only less then N_MAX places
-                        int count;
-                        if (likelyPlaces.getPlaceLikelihoods().size() < N_MAX_ENTRIES) {
-                            count = likelyPlaces.getPlaceLikelihoods().size();
-                        } else {
-                            count = N_MAX_ENTRIES;
-                        }
-
-                        int i = 0;
-                        likelyPlaceNames = new String[count];
-                        likelyPlaceAddresses = new String[count];
-                        likelyPlaceAttributions = new List[count];
-                        likelyPlaceLatLngs = new LatLng[count];
-
-                        for (PlaceLikelihood placeLikelihood : likelyPlaces.getPlaceLikelihoods()) {
-                            // Build a list of likely places to show the user.
-                            likelyPlaceNames[i] = placeLikelihood.getPlace().getName();
-                            likelyPlaceAddresses[i] = placeLikelihood.getPlace().getAddress();
-                            likelyPlaceAttributions[i] = placeLikelihood.getPlace()
-                                    .getAttributions();
-                            likelyPlaceLatLngs[i] = placeLikelihood.getPlace().getLatLng();
-
-                            i++;
-                            if (i > (count - 1)) {
-                                break;
-                            }
-                        }
-
-                        // Show a dialog offering the user the list of likely places, and add a
-                        // marker at the selected place.
-                        // TODO do we even need this?
-                        MapsActivity.this.openPlacesDialog();
-                    }
-                    else {
-                        Log.e(TAG, "Exception: %s", task.getException());
-                    }
-                }
-            });
-        } else {
-            // The user has not granted permission.
-            Log.i(TAG, "The user did not grant location permission.");
-
-            // Add a default marker, because the user hasn't selected a place.
-            map.addMarker(new MarkerOptions()
-                    .title(getString(R.string.default_info_title))
-                    .position(defaultLocation)
-                    .snippet(getString(R.string.default_info_snippet)));
-
-            // Prompt the user for permission.
-            getLocationPermission();
-        }
-    }
-
-    @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions,
                                            @NonNull int[] grantResults){
-        switch (requestCode) {
-            case PERMISSIONS_ACCESS_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    locationPermissionGranted = true;
-                }
+        // If request is cancelled, the result arrays are empty.
+        if (requestCode == PERMISSIONS_ACCESS_FINE_LOCATION) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationPermissionGranted = true;
             }
         }
         updateLocationUI();
@@ -256,7 +146,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     /**
-     * we find users current location and set the camera to it
+     * we find users current location, set the camera to it and put marker
      */
     private void getDeviceLocation(){
         try {
@@ -268,18 +158,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 locationResult.addOnCompleteListener(this, task -> {
                     if (task.isSuccessful()) {
                         // Set the map's camera position to the current location of the device
-                        lastKnownLocation = task.getResult();
+                        Location lastKnownLocation = task.getResult();
                         if (lastKnownLocation != null) {
-                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                                    new LatLng(lastKnownLocation.getLatitude(),
-                                            lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                            LatLng temp = new LatLng(lastKnownLocation.getLatitude(),
+                                    lastKnownLocation.getLongitude());
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(temp, DEFAULT_ZOOM));
+                            // Clear all markers and put one to current position
+                            map.clear();
+                            map.addMarker(new MarkerOptions().position(temp));
+                            MarkerPoint = new LatLng(lastKnownLocation.getLatitude(),
+                                    lastKnownLocation.getLongitude());
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            map.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+                            map.getUiSettings().setMyLocationButtonEnabled(false);
+                            // clear all markers and put one to default
+                            map.clear();
+                            map.addMarker(new MarkerOptions().position(defaultLocation));
                         }
-                    } else {
-                        Log.d(TAG, "Current location is null. Using defaults.");
-                        Log.e(TAG, "Exception: %s", task.getException());
-                        map.moveCamera(CameraUpdateFactory
-                                .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
-                        map.getUiSettings().setMyLocationButtonEnabled(false);
                     }
                 });
             }
@@ -300,19 +198,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng point) {
-                ChosenPoint = point;
-                map.clear();
-                map.addMarker(new MarkerOptions().position(point));
-                try{
-                    Toast.makeText(getApplicationContext(), geocoder.getFromLocation(point.latitude, point.longitude, 2).toString(), Toast.LENGTH_LONG).show();
-                }
-                catch (IOException ex){
-                    Log.e("geocoder", ex.getMessage());
-                }
-            }
+        // Every time user taps on the screen he will put the marker
+        map.setOnMapClickListener(point -> {
+            MarkerPoint = point;
+            map.clear();
+            map.addMarker(new MarkerOptions().position(point));
+            Toast.makeText(getApplicationContext(), MarkerPoint.toString(), Toast.LENGTH_SHORT).show();
         });
         // turn on the location layer
         updateLocationUI();
