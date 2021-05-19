b220760a2a6068859ad7bb78e3c7e2c67b923e35
Nikita Khoroshavtsev <nikitakhor@gmail.com>
2021-05-19 07:16:37 +0300

    Немного доделал основную активность. Пользователя перенаправляет в настройки для разрешения локации, но пока это работает не совсем хорошо

--- app/src/main/java/com/natifick/geonotes/MainActivity.java
+++ app/src/main/java/com/natifick/geonotes/MainActivity.java
@@ -6,17 +6,22 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.natifick.geonotes.database.Address;
 import com.natifick.geonotes.database.DataBase;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,40 +42,57 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         db = new DataBase(null, null, null, 1);
-        addresses = (Address[])db.getSetOfAddresses().toArray();
+        Set <Address> temp = db.getSetOfAddresses();
 
+        Button butt; // Чтобы штамповать кнопки
         // Держатель для всех этих кнопок
         LinearLayout buttonContainer = findViewById(R.id.ButtonKeeper);
-        // Создадим кучу кнопок у которых id - индекс в массиве адресов
-        Button butt; // Чтобы штамповать кнопки
-        for (int i = 0; i < addresses.length; i++){
-            butt = new Button(this);
-
-            // Даём id и текст состоящий из адреса
-            butt.setId(i);
-            butt.setText(addresses[i].getAddress());
-            butt.setBackground(ContextCompat.getDrawable(this, R.drawable.button_shape));
-            butt.setOnClickListener(v -> {
-                Toast.makeText(this, "А это я ещё не сделал", Toast.LENGTH_SHORT).show();
-            });
-
-            buttonContainer.addView(butt);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0, 0, 0, 20);
+        // Если у нас ещё нет адресов, то нам нечего создавать
+        if (temp.size() != 0){
+            addresses = new Address[temp.size()];
+            temp.toArray(addresses);
+            // Создадим кучу кнопок у которых id - индекс в массиве адресов
+            for (int i = 0; i < addresses.length; i++){
+                butt = new Button(this);
+                // Даём id и текст состоящий из адреса
+                butt.setId(i);
+                butt.setText(addresses[i].getAddress());
+                butt.setSingleLine(false); // перенос текста
+                butt.setBackground(ContextCompat.getDrawable(this, R.drawable.button_shape));
+                butt.setOnClickListener(v -> {
+                    Toast.makeText(this, "А это я ещё не сделал", Toast.LENGTH_SHORT).show();
+                });
+                buttonContainer.addView(butt, layoutParams);
+            }
         }
 
+        // И кнопка добавления нового адреса, она чуть ниже всех остальных
+        layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0, 20, 0, 0);
+
+        // Её остальные параметры почти такие же
         butt = new Button(this);
-        butt.setId(addresses.length);
+        butt.setId(temp.size() == 0 ? 0 : addresses.length);
         butt.setBackground(ContextCompat.getDrawable(this, R.drawable.button_shape));
+        butt.setText("Создать новый адрес");
+        butt.setSingleLine(false);
         butt.setOnClickListener(v -> {
             Intent intent = new Intent(this, CreatePlaceActivity.class);
             startActivity(intent);
         });
+        buttonContainer.addView(butt, layoutParams);
 
+        // Создаём канал уведомлений
         createNotificationChannel();
 
-        setContentView(R.layout.activity_main);
-
+        // Получаем разрешение на локацию
         getLocationPermission();
     }
 
@@ -78,22 +100,50 @@ public class MainActivity extends AppCompatActivity {
      * Запрашиваем разрешение на геолокацию, если нам его ещё не дали
      */
     private void getLocationPermission(){
-        if (android.os.Build.VERSION.SDK_INT >= 30) {
-            if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
-                Toast.makeText(this, "Нам нужно ваше местоположение, чтобы присылать заметки. " +
-                        "Пожалуйста, разрешите делать это в фоне", Toast.LENGTH_LONG).show();
-            }
+        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+            request_permission();
         }
 
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 Manifest.permission.ACCESS_BACKGROUND_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             locationPermissionGranted = true;
-            Toast.makeText(this, "It is somehow granted", Toast.LENGTH_LONG).show();
         } else {
             ActivityCompat.requestPermissions(this,
                     new String[]{android.Manifest.permission.ACCESS_BACKGROUND_LOCATION},
                     PERMISSIONS_ACCESS_BACKGROUND_LOCATION);
+        }
+    }
+
+    /**
+     * Запрашиваем разрешение на местоположение.
+     * Пользователю предлагается перейти в настройки, или уйти из приложения
+     */
+    private void request_permission(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.app_name);
+        builder.setMessage(R.string.permission_request);
+        builder.setCancelable(false);
+
+        // Если разрешение получено - продолжаем
+        builder.setPositiveButton("перейти в настройки", (dialog, which) -> {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)){
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.setData(Uri.fromParts("package", getPackageName(), null));
+                startActivity(intent);
+
+            }
+            else{
+                dialog.cancel();
+            }
+        });
+        // Если не хочет - закрываем приложение
+        builder.setNegativeButton("Закрыть приложение", (dialog, which) -> this.finish());
+
+        AlertDialog alert = builder.create();
+        // Проверка того, что активность ещё не закрыта
+        if (!(this).isFinishing()) {
+            alert.show();
         }
     }
 
